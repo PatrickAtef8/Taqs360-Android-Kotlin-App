7c14016a4b0bf70f8aaad3d2bc034291414913c4
Patrick Atef <patrickatefffa22@gmail.com>
2025-05-22 14:57:21 +0300

    Added Weather Ui data needed for some UI looking to be used in the weather view

---
+++ Taqs360/app/src/main/java/com/example/taqs360/home/model/uidata/WeatherUiData.kt
@@ -0,0 +1,28 @@
+package com.example.taqs360.home.model.uidata
+
+import com.example.taqs360.home.model.pojo.Forecast
+
+data class WeatherUiData(
+    val location: String,
+    val currentTemp: Int,
+    val feelsLike: Int,
+    val description: String,
+    val humidity: Int,
+    val windSpeed: Float,
+    val visibility: Int,
+    val date: String,
+    val currentTime: String,
+    val currentWeatherIcon: Int,
+    val forecasts: List<ForecastUiModel>,
+    val timezoneOffset: Int
+)
+
+data class ForecastUiModel(
+    val day: String,
+    val minTemp: Int,
+    val maxTemp: Int,
+    val weatherDescription: String,
+    val iconResId: Int,
+    val forecastsForDay: List<Forecast>,
+    val isToday: Boolean
+)
