f59f99f2a08880bce04f1176bd577878300cd1e0
Patrick Atef <patrickatefffa22@gmail.com>
2025-05-22 14:55:24 +0300

    my pojo is here

---
+++ Taqs360/app/src/main/java/com/example/taqs360/home/model/pojo/WeatherDataClasses.kt
@@ -0,0 +1,41 @@
+package com.example.taqs360.home.model.pojo
+
+data class WeatherResponse(
+    val cod: String,
+    val message: Int,
+    val cnt: Int,
+    val list: List<Forecast>,
+    val city: City
+)
+
+data class Forecast(
+    val dt: Long,
+    val main: Main,
+    val weather: List<Weather>,
+    val wind: Wind,
+    val visibility: Int,
+    val dt_txt: String
+)
+
+data class Main(
+    val temp: Float,
+    val feels_like: Float,
+    val humidity: Int
+)
+
+data class Weather(
+    val id: Int,
+    val main: String,
+    val description: String,
+    val icon: String
+)
+
+data class Wind(
+    val speed: Float
+)
+
+data class City(
+    val name: String,
+    val country: String,
+    val timezone: Int
+)
