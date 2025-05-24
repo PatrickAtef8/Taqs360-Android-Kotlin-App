3c48a7d677cc771f54b78ce070bf27a98149da19
Patrick Atef <patrickatefffa22@gmail.com>
2025-05-24 21:12:39 +0300

    Edited Weather data source to include settings repository for getting units and lang

--- Taqs360/app/src/main/java/com/example/taqs360/home/model/network/WeatherRemoteDataSourceImpl.kt
+++ Taqs360/app/src/main/java/com/example/taqs360/home/model/network/WeatherRemoteDataSourceImpl.kt
@@ -1,10 +1,15 @@
 package com.example.taqs360.home.model.network
 
 import com.example.taqs360.home.model.pojo.WeatherResponse
+import com.example.taqs360.settings.model.repository.SettingsRepository
 
-
-class WeatherRemoteDataSourceImpl(private val weatherService: WeatherService) :
-    WeatherRemoteDataSource {
-    override suspend fun getFiveDayForecast(latitude: Double, longitude: Double, apiKey: String): WeatherResponse =
-         weatherService.fetchFiveDayForecast(latitude, longitude, apiKey)
+class WeatherRemoteDataSourceImpl(
+    private val weatherService: WeatherService,
+    private val settingsRepository: SettingsRepository // Inject SettingsRepository
+) : WeatherRemoteDataSource {
+    override suspend fun getFiveDayForecast(latitude: Double, longitude: Double, apiKey: String): WeatherResponse {
+        val units = settingsRepository.getApiUnits()
+        val language = settingsRepository.getLanguage()
+        return weatherService.fetchFiveDayForecast(latitude, longitude, apiKey, units, language)
     }
+}
