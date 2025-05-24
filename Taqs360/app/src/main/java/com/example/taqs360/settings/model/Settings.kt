5838b34c11b3af58b8de9238d61a5e82050a3dd1
Patrick Atef <patrickatefffa22@gmail.com>
2025-05-24 20:42:38 +0300

    Added my Settings pojo

---
+++ Taqs360/app/src/main/java/com/example/taqs360/settings/model/Settings.kt
@@ -0,0 +1,8 @@
+package com.example.taqs360.settings.model
+
+data class Settings(
+    val temperatureUnit: String,
+    val windSpeedUnit: String,
+    val language: String,
+    val locationMode: String
+)
